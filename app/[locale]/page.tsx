09b8be83b9cb78f231c5748796e4398268711389
HungTP-Play <hungtp.play@gmail.com>
2024-11-12 22:08:57 +0700

    feat: add metadata

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,10 @@
+import { DEFAULT_METADATA } from "@/misc/constants";
 import HomePage from "@components/pages/home-page";
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return DEFAULT_METADATA;
+}
 
 export default function Home() {
   return <HomePage />;
